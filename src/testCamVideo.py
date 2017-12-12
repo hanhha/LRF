adf9561ca4b48bf0a12a8bb67a7fc95091c485d0
Hanh Ha <haminhtranhanh@gmail.com>
2017-12-12 23:43:35 +0000

    Adding info to show up

--- src/testCamVideo.py
+++ src/testCamVideo.py
@@ -5,8 +5,16 @@ import time
 import pygame
 from pygame.locals import *
 import numpy as np
+import os
+
+os.environ['SDL_VIDEODRIVER'] = 'fbcon'
 
 imgDimensions = (320, 240)
+
+firstFrame = None
+secondFrame = None
+thirdFrame = None
+forthFrame = None
 
 camera = picam.PiCamera()
 camera.resolution = imgDimensions 
@@ -16,25 +24,47 @@ rawCapture = PiRGBArray(camera, size=imgDimensions)
 
 screen = None
 
+clock = pygame.time.Clock ()
+
 def initializePygame ():
-	global screen
+	global screen, myfont
+	global firstFrame, secondFrame, thirdFrame, forthFrame
+
 	pygame.init()
 	infoObj = pygame.display.Info ()
 	pygame.mouse.set_visible (False)
 	screen = pygame.display.set_mode ((infoObj.current_w, infoObj.current_h), 0, 32)
+	# Using default system font so that set it None
+	myfont = pygame.font.SysFont (None, 30)
+	firstFrame = (0,0)
+	secondFrame = (infoObj.current_w / 2, 0)
+	thirdFrame = (0,infoObj.current_h)
+	forthFrame = (infoObj.current_w, infoObj.current_h)
 
 def stickImg (img, update = False):
 	pygameSurface = pygame.surfarray.make_surface (img.swapaxes(0,1))
-	screen.blit (pygameSurface, (0,0))
+	screen.blit (pygameSurface, firstFrame)
 	if update:
 		pygame.display.update()
+
+def stickText (text, update = False):
+	textSurface = myfont.render (text, True, (255,255,255))
+	screen.blit (textSurface, secondFrame + (10,10))
+	if update:
+		pygame.display.update()
+
+def cleanScreen ():
+	screen.fill ((0,0,0))
 
 def __main__ ():
 	initializePygame ()
 	for frame in camera.capture_continuous (rawCapture, format ="bgr", use_video_port=True):
 		image = np.copy (frame.array) 
-		stickImg (image, update = True)
-
+		cleanScreen ()
+		stickImg (image)
+		stickText ('FPS ' + str(clock.get_fps()), update = True)
+		
+		clock.tick ()
 		for event in pygame.event.get ():
 			if event.type == QUIT:
 				pygame.quit ()
